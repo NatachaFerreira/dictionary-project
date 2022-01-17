18646161d06ff103a7debb74f133015435bb9922
Natacha_Ferreira <natacha.a.ferreira@aktm.pt>
2022-01-17 08:41:37 -0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <button className='btn btn-primary'>hello</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
