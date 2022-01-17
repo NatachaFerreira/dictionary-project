8035b22596daba5ec08821bcc91746d309fcd17d
Natacha_Ferreira <natacha.a.ferreira@aktm.pt>
2022-01-17 19:16:12 -0100

    rendered the meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
+import "./Dictionary.css";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
+    let [result, setResults] = useState(null);
 
     function handleResponse(response) {
-        console.log(response.data[0]);
+        setResults(response.data[0]);
     }
 
     function search(event) {
         event.preventDefault();
         //API documentation https://dictionaryapi.dev/
-        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`
         axios.get(apiUrl).then(handleResponse);
     }
 
@@ -22,6 +25,7 @@ export default function Dictionary() {
     return <div className="Dictionary">
         <form onSubmit={search}>
             <input type="search" onChange={handleKeywordChange} />
+            <Results results={result} />
         </form>
     </div>
 }
